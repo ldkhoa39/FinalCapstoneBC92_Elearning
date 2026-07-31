ee84e63d216fea8b481b81b483659b4ef07d79a3
ldkhoa39 <lydangkhoa555@gmail.com>
2026-08-01 04:04:22 +0700

    fix bug

--- src/pages/AdminTemplate/Dashboard/index.tsx
+++ src/pages/AdminTemplate/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 // src/pages/AdminTemplate/Dashboard/index.tsx
-import React, { useEffect }from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Dashboard: React.FC = () => {
 
   // useEffect(() => {
   //   console.log("✅ Dashboard Mounted");
-  // }, []);
+  // }, []);     
 
   const stats = [
     {
